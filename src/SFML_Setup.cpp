5744f09439b63c6bbf011bed632f0e03622a70ce
BitzaWolf (Anthony Pepe) <bitzah@gmail.com>
2015-08-19 12:15:18 -0700

    Removed VS2013 project
    Running a VS2013 project in git hub isn't practical

---
+++ src/SFML_Setup.cpp
@@ -0,0 +1,32 @@
+// SFML_Setup.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(400, 300), "SFML Works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
+
