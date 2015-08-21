b150668aff4c6065cf22e0be93a132b20ad2dae2
BitzaWolf (Anthony Pepe) <bitzah@gmail.com>
2015-08-20 20:06:13 -0700

    OpenGL is all setup
    Clone a branch project, then edit that one and such.

--- src/SFML_Setup.cpp
+++ src/SFML_Setup.cpp
@@ -2,29 +2,72 @@
 //
 
 #include "stdafx.h"
-#include <SFML/Graphics.hpp>
-
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(400, 300), "SFML Works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Red);
+	using namespace sf;
 
-	while (window.isOpen())
+	ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	settings.attributeFlags = 0;
+	settings.depthBits = 24;
+	settings.majorVersion = 4;
+	settings.minorVersion = 2;
+	settings.stencilBits = 8;
+
+	Window window(VideoMode(800, 600), "OpenGL", Style::Default, settings);
+	window.setVerticalSyncEnabled(true);
+
+	float red = 1.0;
+	float green = 0;
+	float blue = 0;
+
+	bool running = true;
+	while (running)
 	{
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 			{
-				window.close();
+				running = false;
+			}
+			else if (event.type == Event::Resized)
+			{
+				glViewport(0, 0, event.size.width, event.size.height);
+			}
+			else if (event.type == Event::KeyPressed)
+			{
+				Keyboard::Key key = event.key.code;
+				if (key == Keyboard::Num1)
+				{
+					red = 1.0;
+					green = 0.0;
+					blue = 0.0;
+				}
+				else if (key == Keyboard::Num2)
+				{
+					red = 0.0;
+					green = 1.0;
+					blue = 0.0;
+				}
+				else if (key == Keyboard::Num3)
+				{
+					red = 0.0;
+					green = 0.0;
+					blue = 1.0;
+				}
 			}
 		}
 
-		window.clear();
-		window.draw(shape);
+		glClearColor(red, green, blue, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		window.display();
+
+		sleep(milliseconds(8));
 	}
 
 	return 0;
